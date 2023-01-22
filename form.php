0fe5b6062fc12f4d0e9a0f447f2b456f7641293f
santisans123 <santitok21@gmail.com>
2023-01-22 20:02:59 +0700

    first commit

---
+++ form.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - MYSQL - CRUD</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <section>
+        <h1 style="text-align: center;margin: 50px 0;">PHP CRUD operations with MySQL</h1>
+        <div class="container">
+            <form action="add_data.php" method="post">
+               <div class="row">
+                    <div class="form-group col-lg-4">
+                        <label for="">Student Name</label>
+                        <input type="text" name="name" id="name" class="form-control" required>
+                    </div>
+                    <div class="form-group  col-lg-3">
+                        <label for="">Grade</label>
+                        <select name="grade" id="grade" class="form-control" required>
+                            <option value="">Select a Grade</option>
+                            <option value="grade6">Grade 6</option>
+                            <option value="grade7">Grade 7</option>
+                            <option value="grade8">Grade 8</option>
+                            <option value="grade9">Grade 9</option>
+                            <option value="grade10">Grade 10</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-3">
+                        <label for="">Marks</label>
+                        <input type="number" name="marks" id="marks" class="form-control" required>
+                    </div>
+                    <div class="form-group col-lg-2" style="display: grid;align-items:  flex-end;">
+                        <input type="submit" name="submit" id="submit" class="btn btn-primary">
+                    </div>
+               </div>
+            </form>
+        </div>
+    </section>
+</body>
+
+</html>
